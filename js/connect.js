583f1795b2ea5e28d8af602bad49e214ed15991c
pir0zh0k <127873845+pir0zh0k@users.noreply.github.com>
2023-09-29 22:24:51 +0300

    Update connect.js

--- js/connect.js
+++ js/connect.js
@@ -1,5 +1,5 @@
 const connect_button = document.querySelector("#connect");
 
 connect_button.addEventListener("click", () => {
-    location.href("https://t.me/pir0zh0kk");
+    location.href = "https://t.me/pir0zh0kk";
 });
