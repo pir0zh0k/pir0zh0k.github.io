899d4387ab649068270fac9302416f6f14e37769
pir0zh0k <127873845+pir0zh0k@users.noreply.github.com>
2023-09-29 21:48:48 +0300

    Update anchors and smooth_scroll

--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -1,9 +1,10 @@
 const anchors = document.querySelectorAll('a[href*="#"]');
+const mobile_menu = document.querySelector(".mobile__menu");
 
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
-    
+    mobile_menu.classList.remove("active");
     const blockID = anchor.getAttribute('href').substr(1);
     
     document.getElementById(blockID).scrollIntoView({
